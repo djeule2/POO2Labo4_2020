b6d5a0033dfc0a8d1379a16d2bacfeed3b5964f5
ClaudeAlves <claudeuw@hotmail.com>
2020-06-04 12:07:41 +0200

    time in stats

--- src/Stats.cpp
+++ src/Stats.cpp
@@ -4,11 +4,13 @@
 
 #include <sstream>
 #include "Stats.h"
+#include <chrono>
 
 string Stats::produceStats(size_t simSize, Field *field) {
     size_t numberOfVictorySims = 0;
     std::ostringstream stream;
     field->reset();
+    const auto start = std::chrono::system_clock::now();
     for(size_t i = 0; i < simSize; ++i) {
         field->reset();
         while(field->isGameOver() == 0) {
@@ -18,6 +20,9 @@ string Stats::produceStats(size_t simSize, Field *field) {
             numberOfVictorySims++;
         }
     }
-    stream << ((double)numberOfVictorySims/(double)simSize*100.0);
+    const auto stop = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = stop-start;
+    stream << ((double)numberOfVictorySims/(double)simSize*100.0) << "% temps de simulation : "
+            << diff.count() << " s";
     return stream.str();
 }
